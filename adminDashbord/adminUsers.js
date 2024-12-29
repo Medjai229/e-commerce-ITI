9c2b1ed89c949ba85f5a478c9deb1687f5f51813
Malik <malikhussein229@gmail.com>
2024-12-29 17:15:23 +0200

    Updated the change role bttn to change the user to an admin or user

--- adminDashbord/adminUsers.js
+++ adminDashbord/adminUsers.js
@@ -6,70 +6,57 @@ xhr.addEventListener('readystatechange', function () {
   if (xhr.readyState == 4 && xhr.status == 200) {
     var data_json = JSON.parse(xhr.response);
     console.log(JSON.parse(xhr.response));
-    // var my_id = lastid(data_json);
-    // localStorage.setItem('id', my_id);
     getAllProducts(data_json);
   }
 });
 
-// function allData() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', `http://localhost:3000/users`);
-//   xhr.send();
-//   var theDataa;
-//   xhr.addEventListener('readystatechange', function () {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//       theDataa = JSON.parse(xhr.response);
-//       console.log(JSON.parse(xhr.response));
-//     }
-//   });
-//   return theDataa;
-// }
-
-// function lastid(ele) {
-//   var id_num = ele[ele.length - 1].id;
-//   console.log(id_num);
-//   return id_num;
-// }
-
-// function addCategory() {
-//   var post = new XMLHttpRequest();
-//   post.open("POST", `../productData.json`);
-//   post.setRequestHeader("Content-Type", "application/json");
-//     let data = JSON.stringify({
-//       id: Number(localStorage.getItem("id")) + 1 + "",
-//       title: title_value,
-//       description: dec_value,
-//     });
-//     post.send(data);
-//   }
-// }
-
 // ! refactor with create element
 function getAllProducts(elem) {
   var note = `  
-  <thead>
+              <thead> 
                 <tr>
+                  <th>ID</th>
                   <th>Title</th>
                   <th>Price</th>
                   <th>Admin</th>
                   <th>Action</th>
-
                 </tr>
               </thead>`;
   for (let i = 0; i < elem.length; i++) {
     // ! in the admin section make display user or admin
+    // ! in line 35 match the class name with the button
     note += ` <tbody>
                 <tr>
+                  <td>${elem[i].id}</td>
                   <td>${elem[i].fullname}</td>
                   <td>${elem[i].email}</td>
                   <td>${elem[i].isAdmin}</td>
                   <th class="adit_delete">
-                    <button onclick="openPopUpUpdate(${elem[i].id})">Edit</button>
-                    <button>Update</button>
+                    <button onclick= "changeRole(event)">Change Role</button>
                   </th>
                 </tr>
               </tbody>`;
   }
   document.getElementById('product_table').innerHTML = note;
+}
+
+function changeRole(event) {
+  let isAdmin = true;
+  let userRole = event.target.parentElement.previousElementSibling.textContent;
+  if (userRole === 'true') {
+    isAdmin = false;
+  }
+  let userId =
+    event.target.parentElement.parentElement.firstElementChild.textContent;
+  console.log(userId);
+  fetch(`http://localhost:3000/users/${userId}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      isAdmin: isAdmin,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
